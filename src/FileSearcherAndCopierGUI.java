ec8b3a3a88ec49f8f977e2ebd0322815fe924a37
tomer_h <tomer2b0525@gmail.com>
2025-01-07 15:58:37 +0200

    added checkboxes components, added help button to open help window

--- src/FileSearcherAndCopierGUI.java
+++ src/FileSearcherAndCopierGUI.java
@@ -22,50 +22,89 @@ public class FileSearcherAndCopierGUI {
     private JTextField destinationDirectoryPath2;
     private JLabel destinationDirectoryPath2Label;
     private JCheckBox searchDuplicates;
-
+    private JLabel searchDuplicatesLabel;
+    private JCheckBox exactMatch;
+    private JLabel exactMatchLabel;
+    private JButton helpButton;
 
     public FileSearcherAndCopierGUI() {
-        frame = new JFrame("File Searcher and Copier");
+        frame = new JFrame("Search and copy files");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setMinimumSize(new Dimension(500, 375));
+        frame.setPreferredSize(new Dimension(500, 400));
+        frame.pack();
         frame.setResizable(Boolean.FALSE);
 
-        JPanel firstPanel = new JPanel();
+
+        helpButton = new JButton("help");
+        JPanel panel4 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        helpButton.setPreferredSize(new Dimension(55,20));
+        helpButton.setFont(new Font(helpButton.getFont().getName(),Font.BOLD,10));
+
+        helpButton.addActionListener(actionListenenr -> {
+            JOptionPane.showMessageDialog(frame,
+                    "to begin searching files to copy please provide the next things: \n " +
+                    "1.Source directory to search the files \n " +
+                    "2.Destination directory to copy and paste the found files \n " +
+                    "3.the strings to search in source directory"
+            ,"Help",JOptionPane.QUESTION_MESSAGE);
+        });
+        panel4.add(helpButton);
+        frame.add(BorderLayout.NORTH,panel4);
+
+        JPanel firstPanel = new JPanel(new FlowLayout());
 
         searchStringsTextArea = new JTextArea(8, 20);
+
         JScrollPane scrollPane = new JScrollPane(searchStringsTextArea);
-        JTextPane scrollpaneLabel = new JTextPane();
-        scrollpaneLabel.setText("to begin searching files to copy please provide the next things: \n 1.Source directory to search the files \n 2.Destination directory to copy and paste the found files \n 3.the strings to search in source directory");
-        firstPanel.add(scrollpaneLabel);
         firstPanel.add(scrollPane);
 
-        JPanel directoryPanel = new JPanel();
+        JPanel flagsPanel = new JPanel(new GridLayout(0,2,5,5));
+
+        searchDuplicates = new JCheckBox();
+        searchDuplicatesLabel = new JLabel("Search duplicates?");
+        searchDuplicatesLabel.setLabelFor(searchDuplicates);
+        flagsPanel.add(searchDuplicatesLabel);
+        flagsPanel.add(searchDuplicates);
+
+        exactMatch = new JCheckBox();
+        exactMatchLabel = new JLabel("exact match only?");
+        exactMatchLabel.setLabelFor(exactMatch);
+        flagsPanel.add(exactMatchLabel);
+        flagsPanel.add(exactMatch);
+
+
+        firstPanel.add(flagsPanel);
+
+
+        JPanel directoryPanel = new JPanel(new GridLayout(0,2,5,0));
 
         sourceDirectoryPath2 = new JTextField();
         sourceDirectoryPath2.setColumns(20);
-        sourceDirectoryPath2Label = new JLabel("Directory to search in : ");
+        sourceDirectoryPath2Label = new JLabel();
         sourceDirectoryPath2Label.setText("Directory to search in : ");
 
         sourceDirectoryPath2Label.setLabelFor(sourceDirectoryPath2);
 
         JPanel directorySourcePanel = new JPanel();
-        directorySourcePanel.add(sourceDirectoryPath2Label);
-        directorySourcePanel.add(sourceDirectoryPath2);
+        directoryPanel.add(sourceDirectoryPath2Label);
+        directoryPanel.add(sourceDirectoryPath2);
 
-        firstPanel.add(directorySourcePanel);
+//        directoryPanel.add(directorySourcePanel);
 
         destinationDirectoryPath2 = new JTextField();
         destinationDirectoryPath2.setColumns(20);
-        destinationDirectoryPath2Label = new JLabel("Directory to search in : ");
+        destinationDirectoryPath2Label = new JLabel();
         destinationDirectoryPath2Label.setText("Destination folder to copy to : ");
 
         destinationDirectoryPath2Label.setLabelFor(destinationDirectoryPath2);
 
         JPanel directoryDestinationPanel = new JPanel();
-        directoryDestinationPanel.add(destinationDirectoryPath2Label);
-        directoryDestinationPanel.add(destinationDirectoryPath2);
+        directoryPanel.add(destinationDirectoryPath2Label);
+        directoryPanel.add(destinationDirectoryPath2);
 
-        firstPanel.add(directoryDestinationPanel);
+//        directoryPanel.add(directoryDestinationPanel);
+
+        firstPanel.add(directoryPanel);
 
         searchAndCopyButton = new JButton("Search and Copy");
         firstPanel.add(searchAndCopyButton);
@@ -82,6 +121,7 @@ public class FileSearcherAndCopierGUI {
 
         firstPanel.add(directoryPanel);
         frame.add(firstPanel);
+        searchStringsTextArea.grabFocus();
         frame.setVisible(true);
 
     }
@@ -93,14 +133,17 @@ public class FileSearcherAndCopierGUI {
         //creating FileFilter to use instead of iterating normaly...
         //testing performance and speed
         FileFilter fileFilter = file -> {
-            if (file.isFile() && !searchStrings.isEmpty() && !file.getName().toLowerCase().endsWith(".txt")) {
+            System.out.println(searchDuplicates.isSelected());
+            if (file.isFile() && !searchStrings.isEmpty() && file.getName().toLowerCase().endsWith(".txt")) {
 
                 for (int i = 0; i < searchStrings.size(); i++) {
+//                    if (exactMatch)
                     if (file.getName().contains(searchStrings.get(i))) {
-//
-//                            if (!searchDuplicates) {
-//                                searchStrings.remove(i);
-//                            }
+
+                            if (!searchDuplicates.isSelected()) {
+                                searchStrings.remove(i);
+                            }
+
                         return true;
                     }
                 }
